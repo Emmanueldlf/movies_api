d08a87d68068ef621b91e2ce24405e54b1915173
Emmanuel de La Forest <emmanuel.delaforestdivonne@gmail.com>
2023-05-31 21:31:41 +0900

    added an image field

--- movies/models.py
+++ movies/models.py
@@ -11,3 +11,4 @@ class MovieData(models.Model):
     duration = models.FloatField()
     rating = models.FloatField()
     category = models.CharField(max_length=200,default='comedy')
+    image = models.ImageField(upload_to='Images/',default='Images/None/Noimg.jpg')
--- movies_api/urls.py
+++ movies_api/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import include,path
 from rest_framework import routers
 from movies.views import MovieViewSet,ComedyViewSet,ActionViewSet
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 router = routers.SimpleRouter()
 router.register('movies',MovieViewSet)
@@ -29,4 +30,4 @@ router.register('action',ActionViewSet)
 urlpatterns = [
     path('',include(router.urls)),
     path('admin/', admin.site.urls),
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
